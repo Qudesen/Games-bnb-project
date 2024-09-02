8672c9a4b407d2ebe1cc7fe2498d0420e68e7d67
Quentin Desgrippes <desgrippes.quentin@gmail.com>
2024-09-02 15:55:20 +0200

    Adding controllers, views & actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :games
+  resources :rentals, :except [:new]
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
