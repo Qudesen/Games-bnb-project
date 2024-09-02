f1d7811c81d96d3cae8e4f6c6cddb21c08356b8e
redhaboukoult <redha.boukoult@gmail.com>
2024-09-02 17:08:38 +0200

    En cours...

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :games
-  resources :rentals, :except [:new]
+  resources :rentals, except: [:new]
 
   # Defines the root path route ("/")
   # root "posts#index"
