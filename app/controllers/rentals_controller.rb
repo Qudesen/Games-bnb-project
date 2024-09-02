049b7b80f8f42cf71ab3d4da86a58a5844c74e4c
Sylvain Dubois <sylvain.dubois12@gmail.com>
2024-09-02 16:59:27 +0200

    controllers games and rentals ok

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,19 +1,44 @@
 class RentalsController < ApplicationController
+
   def index
+    @rentals = Rental.all
   end
 
   def show
+    @rental = Rental.find(params[:id])
   end
 
   def create
+    @rental = Rental.new(rental_params)
+    if @rental.save!
+      redirect_to rental_path(@rental)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @rental = Rental.find(params[:id])
   end
 
   def update
+    @rental = Rental.find(params[:id])
+    if @rental.update!(rental_params)
+      redirect_to rental_path(@rental)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @rental = Rental.find(params[:id])
+    @rental.destroy
+    redirect_to rentals_path, status: :see_other
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, :status, :game_id, :user_id)
   end
 end
