400874f186d6880bf9e18da58fbe9388d48d099c
Quentin Desgrippes <desgrippes.quentin@gmail.com>
2024-09-03 12:19:27 +0200

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,85 @@
 require "faker"
 
 puts "Processing seeds..."
+puts "Destroying all records..."
 
+Rental.destroy_all
+Game.destroy_all
+User.destroy_all
+
+puts "All records destroyed!"
+puts "Creating users"
 # Create 10 users
+
+sylvain = User.create!(
+  email: "sylvain@gmail.com",
+  username: "sylvaind",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+redha = User.create!(
+  email: "redhab@gmail.com",
+  username: "redhab",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+pakal = User.create!(
+  email: "pakal@gmail.com",
+  username: "pakalh",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+ali = User.create!(
+  email: "ali@gmail.com",
+  username: "aliad",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+jeremie = User.create!(
+  email: "jeremie@gmail.com",
+  username: "jeremiel",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+quentin = User.create!(
+  email: "quentin@gmail.com",
+  username: "quentind",
+  phone_number: "06 01 02 03 04",
+  password: 123456
+)
+
+puts "Creating games"
+
+# Create 10 games
 10.times do
-  User.create(
-    email: Faker::Internet.email,
-    username: Faker::Internet.username,
-    phone_number: Faker::PhoneNumber.phone_number,
-    password: "123456")
+  game = Game.new(
+    name: ["Echec", "Monopoly", "Uno", "Poker", "Tarot", "Belote", "Scrabble", "Dixit", "Mille Bornes", "Pictionary"].sample,
+    description: ["Voici une description du jeu, ce jeu est un jeu de société très amusant", "Voici une description du jeu, ce jeu est un jeu de société très amusant, Voici une description du jeu, ce jeu est un jeu de société très amusant."].sample,
+    price_per_day: [1.2, 2.3, 3.5, 6.8].sample,
+    address: [" 1 rue de la République, Aix en provence", "2 bd longchamp, marseille", "10 av des Champs Elysées, Paris"].sample
+) 
+  game.user = User.all.sample
+  game.save
 end
 
-# Create 20 rentals
-20.times do
-  Rental.create(
+puts "Creating rentals"
+
+# Create 10 rentals
+10.times do
+  rental = Rental.new(
     price: Faker::Number.decimal(l_digits: 2),
     start_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
     end_date: Faker::Date.between(from: Date.today, to: 2.days.from_now),
-    user_id: Faker::Number.within(range: 1..10),
-    game_id: Faker::Number.within(range: 1..10),
     status: ["pending", "accepted", "rejected"].sample,
 )
+  rental.user = User.all.sample
+  rental.game = Game.all.sample
+  rental.save
 end
 
 puts "Seeds processed!"
