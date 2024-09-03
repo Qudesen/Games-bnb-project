2319a4310a3ec9510acf82a6c1cc1dd7d766df4b
Sylvain Dubois <sylvain.dubois12@gmail.com>
2024-09-03 17:58:17 +0200

    create rental ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,7 @@ class GamesController < ApplicationController
 
   def index
     @games = Game.all
+    @rental = Rental.new
   end
 
   def show
---
+++ app/javascript/controllers/price_calculator_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="price-calculator"
+export default class extends Controller {
+  static targets = ["price", "startDate", "endDate"]
+  static values = { price: Number }
+
+  calculate() {
+    console.log("on a atteint l'action")
+    const start = this.startDateTarget.value
+    const end = this.endDateTarget.value
+    const startDate = new Date(start)
+    const endDate = new Date(end)
+    const result = ( endDate - startDate ) / (1000 * 60 * 60 * 24)
+    this.priceTarget.innerHTML = this.priceValue * result
+  }
+}
